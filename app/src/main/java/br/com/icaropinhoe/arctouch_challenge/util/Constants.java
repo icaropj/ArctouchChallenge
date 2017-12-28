fb5f6c4487bb0151fafe1208f8fd83e0ae9ce1ee
Ícaro Pinho Esmeraldo <icaropinhoe@gmail.com>
2017-12-28 14:11:24 -0200

    Util classes

---
+++ app/src/main/java/br/com/icaropinhoe/arctouch_challenge/util/Constants.java
@@ -0,0 +1,17 @@
+package br.com.icaropinhoe.arctouch_challenge.util;
+
+/**
+ * Created by icaro on 27/12/2017.
+ */
+
+public class Constants {
+
+    public static final String API_KEY = "1f54bd990f1cdfb230adb312546d765d";
+
+    public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
+
+    public static final String UPCOMING = BASE_URL + "upcoming?language=en-US";
+
+    public static final String SMALL_IMAGE_PATH = "https://image.tmdb.org/t/p/w92";
+    public static final String LARGE_IMAGE_PATH = "https://image.tmdb.org/t/p/w300";
+}
